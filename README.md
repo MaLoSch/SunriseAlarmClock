3c98a52bbd2c6e5835d00bb39d0588192a3fc247
Markus Lorenz Schilling <hey@malosch.com>
2016-05-17 16:45:24 -0700

    20160517 - Update
    Added button to Arduino Code
    Encoder now controls brightness of LEDs
    Encoder is now constrained to values of 0-10
    Added ToDo List and Whislist to readme.md

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # SunriseAlarmClock
 Files to build and code the DIY Sunrise Alarm Clock
+
+ToDo's
+- Add asynchrone code structure
+- Clean up code
+	- remove unnecessary variables
+	- tighten up code
+	- create tabs for single methods
+- Set brightness of light with encoder
+	- 0 = OFF
+	- 1-10 = Brightness levels
+- Set alarm time
+	- Turn on light on alarm
+- Simulate sunrise
+- Display alarm time (in hours)
+- Feedback when alarm is set through button LED ring lighting up
+- ...
+
+Wishlist
+- Add sound capability
+	- https://www.adafruit.com/products/94
+	- https://www.adafruit.com/products/1788
+	- https://www.adafruit.com/products/1790
+	- https://www.adafruit.com/products/175
+- Add Ethernet capability
+	- https://www.adafruit.com/products/418
--- SRAC/SRAC.ino
+++ SRAC/SRAC.ino
@@ -20,6 +20,7 @@
 const int lowerLight_Pin = 5; // Pin # for lower light
 const int rightLight_Pin = 6; // Pin # for lower light
 const int numPixelPerLight = 12; // # of pixels per LED strip
+const int buttonPin = 12;
 
 /* NEOPIXEL */
 CRGB bottomLight[numPixelPerLight]; // Bottom LED strip
@@ -34,9 +35,19 @@ int currentMinute;
 int alarmHour;
 int alarmMinute;
 
+/* LIGHT */
+int c_hue = 0;
+int c_sat = 0;
+int c_val = 0;
+
+/* BUTTON */
+int currentButtonState = 0; // variable to store current state of pushbutton
+int prevButtonState = -1; // variable to store previous state of pushbutton
+
 /* ENCODER */
-long prevPosition = -999; // variable to store old position of encoder
-long currentPosition; // variable to store current position of encoder
+long prevPosition = 0; // variable to store old position of encoder
+long currentPosition = -999; // variable to store current position of encoder
+int encVal = 0; // value of encoder (0-10)
 Encoder myEnc(2,3); // Encoder is connected to digital pins 2 and 3 (both of which have interrupt capability)
 //   Best Performance: both pins have interrupt capability
 //   Good Performance: only the first pin has interrupt capability
@@ -44,28 +55,50 @@ Encoder myEnc(2,3); // Encoder is connected to digital pins 2 and 3 (both of whi
 
 void setup() { 
   Serial.begin(9600);
+  pinMode(buttonPin, INPUT); // Initialize buttonPin as an Input
   FastLED.addLeds<WS2812B, lowerLight_Pin, GRB>(bottomLight, numPixelPerLight);
   FastLED.addLeds<WS2812B, rightLight_Pin, GRB>(rightLight, numPixelPerLight);
 }
 
 void loop() {
   readEnc(); // read encoder
-  //readTime(); // read current time
-   //for(uint8_t hue = 0; hue < 255 ; hue = hue + 1) {
-      //for(int ledIndex = 0; ledIndex < numPixelPerLight; ledIndex ++) {
-        //bottomLight[ledIndex] = CHSV(hue,255,255);
-        //rightLight[ledIndex] = CHSV(hue-128,255,255);
-      //}
-      //FastLED.show();
+  //readBttn(); // read button
+  readTime(); // read current time
 }
 
 void readEnc() {
   currentPosition = myEnc.read()/4; // read encoder, divide value by 4 and store it in currentPosition 
   //(since it is a quadrature encoder it puts out the values 1,2,3,4 on one turn but we only want it to put out the value 1)
   if(currentPosition != prevPosition) {
+    if(currentPosition > prevPosition) {
+      encVal ++;
+    } else {
+      encVal --;
+    }
+    encVal = constrain(encVal, 0, 10);
     prevPosition = currentPosition; // if prevPosition is unqual currentPosition we update prevPosition to currentPosition
-    Serial.println(currentPosition); // print currentPosition
+    setLight();
   }
+}
+
+void readBttn() {
+  currentButtonState = digitalRead(buttonPin);
+  if(currentButtonState != prevButtonState) {
+    prevButtonState = currentButtonState;
+    Serial.println(currentButtonState);
+  }
+}
+
+void setLight() {
+
+  c_val = map(encVal,0,10,0,255);
+  
+  for(int ledIndex = 0; ledIndex < numPixelPerLight; ledIndex ++) {
+    bottomLight[ledIndex] = CHSV(c_hue,c_sat,c_val);
+    rightLight[ledIndex] = CHSV(c_hue,c_sat,c_val);
+  }
+  Serial.println(c_val);
+  FastLED.show();
 }
 
 void readTime() {
